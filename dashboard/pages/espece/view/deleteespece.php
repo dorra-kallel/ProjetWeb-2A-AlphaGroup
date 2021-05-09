fbb7a139b4caabe2726841842429889fbe6237e7
tataros <49034600+tataros@users.noreply.github.com>
2021-05-09 23:05:58 +0100

    Delete dashboard directory

--- dashboard/pages/espece/view/deleteespece.php
+++
@@ -1,9 +0,0 @@
- <?php
-   
-    include '../Controller/especeC.php';
-    $especeC = new especeC() ;
-    	if (isset($_POST["id"])){
-		$especeC->supprimerespece($_POST["id"]);
-		header('Location:addespece.php');
-	}
-?>
