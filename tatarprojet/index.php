945e24b654a13e6ab182ce5fce6769d8bc5cdebb
tataros <49034600+tataros@users.noreply.github.com>
2021-05-09 22:49:09 +0100

    Add files via upload

---
+++ tatarprojet/index.php
@@ -0,0 +1,260 @@
+<?php
+include 'inc/header.php';
+
+Session::CheckSession();
+
+$logMsg = Session::get('logMsg');
+if (isset($logMsg)) {
+  echo $logMsg;
+}
+$msg = Session::get('msg');
+if (isset($msg)) {
+  echo $msg;
+}
+Session::set("msg", NULL);
+Session::set("logMsg", NULL);
+?>
+<?php
+
+if (isset($_GET['remove'])) {
+  $remove = preg_replace('/[^a-zA-Z0-9-]/', '', (int)$_GET['remove']);
+  $removeUser = $users->deleteUserById($remove);
+}
+
+if (isset($removeUser)) {
+  echo $removeUser;
+}
+if (isset($_GET['deactive'])) {
+  $deactive = preg_replace('/[^a-zA-Z0-9-]/', '', (int)$_GET['deactive']);
+  $deactiveId = $users->userDeactiveByAdmin($deactive);
+}
+
+if (isset($deactiveId)) {
+  echo $deactiveId;
+}
+if (isset($_GET['active'])) {
+  $active = preg_replace('/[^a-zA-Z0-9-]/', '', (int)$_GET['active']);
+  $activeId = $users->userActiveByAdmin($active);
+}
+
+if (isset($activeId)) {
+  echo $activeId;
+}
+
+
+
+ ?>
+      <div class="card ">
+        <div class="card-header">
+          <h3><i class="fas fa-users mr-2"></i>User list <span class="float-right">Welcome! <strong>
+            <span class="badge badge-lg badge-secondary text-white">
+<?php
+$username = Session::get('username');
+if (isset($username)) {
+  echo $username;
+}
+ ?></span>
+
+          </strong></span></h3>
+          <div id="google_translate_element"></div>
+<script type="text/javascript">
+function googleTranslateElementInit() {
+new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+</script>
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+        <form method="POST" action=""> 
+              Rechercher un mot : <input type="text" name="recherche">
+              <input type="SUBMIT" value="Search!"> 
+              <?php
+
+    $db_server = 'localhost'; // Adresse du serveur MySQL
+    $db_name = '';            // Nom de la base de données
+    $db_user_login = 'root';  // Nom de l'utilisateur
+    $db_user_pass = '';       // Mot de passe de l'utilisateur
+
+    // Ouvre une connexion au serveur MySQL
+    $conn = mysqli_connect($db_server,$db_user_login, $db_user_pass, $db_name);
+
+
+     // Récupère la recherche
+     $recherche = isset($_POST['recherche']) ? $_POST['recherche'] : '';
+
+     // la requete mysql
+     $q = $conn->query(
+     "SELECT id, name FROM tbl_users
+      WHERE id LIKE '%$recherche%'
+      OR name LIKE '%$recherche%'
+      LIMIT 10");
+
+     // affichage du résultat
+     while( $r = mysqli_fetch_array($q)){
+     echo 'Résultat de la recherche: '.$r['id'].', '.$r['name'].' <br />'
+;
+     }
+?>
+        </form>
+        
+        </div>
+
+        
+        <div class="card-body pr-2 pl-2">
+                    
+
+            <table id="example" class="table table-striped table-bordered" style="width:100%">
+
+                  <thead>
+                    <tr>
+                     
+                      <th  class="text-center">SL</th>
+                      <th  class="text-center">Name</th>
+                      <th  class="text-center">Username</th>
+                      <th  class="text-center">Email address</th>
+                      <th  class="text-center">Mobile</th>
+                      <th  class="text-center">Status</th>
+                      <th  class="text-center">Created</th>
+                      <th  width='25%' class="text-center">Action</th>
+                    </tr>
+                    
+                  </thead>
+                  <button class="btn">Print Table</button>
+
+<!-- jQuery library -->
+              <script src="assets/jquery.min.js"></script>
+ <script>
+          $('.btn').click(function(){
+          //alert('yee');
+          var printimp = document.getElementById('table');
+          var imptable = window.print("","","width=900,height=700");
+          imptable.document.write(printimp.outerHTML);
+          imptable.document.close();
+          imptable.focus();
+          imptable.print();
+          imptable.close();
+  });
+ </script>
+                  <tbody>
+                  
+				
+			
+                    <?php
+
+                      $allUser = $users->selectAllUserData();
+
+                      if ($allUser) {
+                        $i = 0;
+                        foreach ($allUser as  $value) {
+                          $i++;
+
+                     ?>
+
+                      <tr class="text-center"
+                      <?php if (Session::get("id") == $value->id) {
+                        echo "style='background:#d9edf7' ";
+                      } ?>
+                      >
+
+                        <td><?php echo $i; ?></td>
+                        <td><?php echo $value->name; ?></td>
+                        <td><?php echo $value->username; ?> <br>
+                          <?php if ($value->roleid  == '1'){
+                          echo "<span class='badge badge-lg badge-info text-white'>Admin</span>";
+                        } elseif ($value->roleid == '2') {
+                          echo "<span class='badge badge-lg badge-dark text-white'>Editor</span>";
+                        }elseif ($value->roleid == '3') {
+                            echo "<span class='badge badge-lg badge-dark text-white'>User Only</span>";
+                        } ?></td>
+                        <td><?php echo $value->email; ?></td>
+
+                        <td><span class="badge badge-lg badge-secondary text-white"><?php echo $value->mobile; ?></span></td>
+                        <td>
+                          <?php if ($value->isActive == '0') { ?>
+                          <span class="badge badge-lg badge-info text-white">Active</span>
+                        <?php }else{ ?>
+                    <span class="badge badge-lg badge-danger text-white">Deactive</span>
+                        <?php } ?>
+
+                        </td>
+                        <td><span class="badge badge-lg badge-secondary text-white"><?php echo $users->formatDate($value->created_at);  ?></span></td>
+
+                        <td>
+                          <?php if ( Session::get("roleid") == '1') {?>
+                            <a class="btn btn-success btn-sm
+                            " href="profile.php?id=<?php echo $value->id;?>">View</a>
+                            <a class="btn btn-info btn-sm " href="profile.php?id=<?php echo $value->id;?>">Edit</a>
+                            <a onclick="return confirm('Are you sure To Delete ?')" class="btn btn-danger
+                    <?php if (Session::get("id") == $value->id) {
+                      echo "disabled";
+                    } ?>
+                             btn-sm " href="?remove=<?php echo $value->id;?>">Remove</a>
+
+                             <?php if ($value->isActive == '0') {  ?>
+                               <a onclick="return confirm('Are you sure To Deactive ?')" class="btn btn-warning
+                       <?php if (Session::get("id") == $value->id) {
+                         echo "disabled";
+                       } ?>
+                                btn-sm " href="?deactive=<?php echo $value->id;?>">Disable</a>
+                             <?php } elseif($value->isActive == '1'){?>
+                               <a onclick="return confirm('Are you sure To Active ?')" class="btn btn-secondary
+                       <?php if (Session::get("id") == $value->id) {
+                         echo "disabled";
+                       } ?>
+                                btn-sm " href="?active=<?php echo $value->id;?>">Active</a>
+                             <?php } ?>
+
+
+
+
+                        <?php  }elseif(Session::get("id") == $value->id  && Session::get("roleid") == '2'){ ?>
+                          <a class="btn btn-success btn-sm " href="profile.php?id=<?php echo $value->id;?>">View</a>
+                          <a class="btn btn-info btn-sm " href="profile.php?id=<?php echo $value->id;?>">Edit</a>
+                        <?php  }elseif( Session::get("roleid") == '2'){ ?>
+                          <a class="btn btn-success btn-sm
+                          <?php if ($value->roleid == '1') {
+                            echo "disabled";
+                          } ?>
+                          " href="profile.php?id=<?php echo $value->id;?>">View</a>
+                          <a class="btn btn-info btn-sm
+                          <?php if ($value->roleid == '1') {
+                            echo "disabled";
+                          } ?>
+                          " href="profile.php?id=<?php echo $value->id;?>">Edit</a>
+                        <?php }elseif(Session::get("id") == $value->id  && Session::get("roleid") == '3'){ ?>
+                          <a class="btn btn-success btn-sm " href="profile.php?id=<?php echo $value->id;?>">View</a>
+                          <a class="btn btn-info btn-sm " href="profile.php?id=<?php echo $value->id;?>">Edit</a>
+                        <?php }else{ ?>
+                          <a class="btn btn-success btn-sm
+                          <?php if ($value->roleid == '1') {
+                            echo "disabled";
+                          } ?>
+                          " href="profile.php?id=<?php echo $value->id;?>">View</a>
+
+                        <?php } ?>
+
+                        </td>
+                      </tr>
+                    <?php }}else{ ?>
+                      <tr class="text-center">
+                      <td>No user availabe now !</td>
+                    </tr>
+                    <?php } ?>
+
+                  </tbody>
+
+              </table>
+
+
+
+
+
+
+
+
+
+        </div>
+      </div>
+
+
+
+  
