89b2004c47ad1b2fc8776d36d2b5fefb1316efe1
azizcherif20 <aziz.cherif1@esprit.tn>
2021-05-12 01:09:07 +0200

    dash commit

--- view/deleteCom.php
+++
@@ -1,9 +0,0 @@
-<?php
-   
-    include '../Controller/commC.php';
-    $commC = new commC() ;
-    	if (isset($_POST["id_com"])){
-		$commC->supprimerComs($_POST["id_com"]);
-		header('Location:addcom.php');
-	}
-?>
